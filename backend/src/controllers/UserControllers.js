6aba4366b2b8ac0216a090633fc8fb3fe949043f
Kaiky Rodrigues <kaikyro11@gmail.com>
2026-01-06 11:07:22 -0300

    fix: handle missing email in request

--- backend/src/controllers/UserControllers.js
+++ backend/src/controllers/UserControllers.js
@@ -3,31 +3,35 @@ import bcrypt from "bcryptjs"
 
 export async function register(req, res) {
     const {name, email, password} = req.body
-    const userAlreadyExists = await prisma.user.findUnique({
-        where: {
-            email
-        }
-    })
-
-    if(!name || !email || !password){
+    // a first verification is necessary to avoid 500: Internal Server Error
+    // when the request does not include an "email"
+    if (!name || !email || !password){
         return res.status(400).json({error: "Missing information"})
     }
 
-    if(userAlreadyExists){
-        return res.status(409).json({error: "Email already in use"})
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const newUser = await prisma.user.create({
-        data: {
-            name,
-            email,
-            password: hashedPassword
+    try{
+        const userAlreadyExists = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+        if(userAlreadyExists){
+            return res.status(409).json({error: "Email already in use"})
         }
-    })
-    return res.status(201).json({
-        id: newUser.id,
-        name: newUser.name,
-        email: newUser.email
-    })
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = await prisma.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: hashedPassword
+            }
+        })
+        return res.status(201).json({
+            id: newUser.id,
+            name: newUser.name,
+            email: newUser.email
+        })
+    }catch(error){
+        return res.status(500).json({error: "Internal server error"})
+    }
 }
