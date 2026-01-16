77f8aa101d6387bd23695b996132017959192d11
Kaiky Rodrigues <kaikyro11@gmail.com>
2026-01-16 10:26:56 -0300

    feat: display task description with tooltip

--- frontend/script/tasks/render/tasks/render.js
+++ frontend/script/tasks/render/tasks/render.js
@@ -4,26 +4,32 @@ export const renderTasks = (taskList) => {
     list.innerHTML = ""
 
     taskList.forEach(task => {
+        const div = document.createElement("div")
 
         const editButton = document.createElement("button")
         const deleteButton = document.createElement("button")
         const doneButton = document.createElement("button")
         const li = document.createElement("li")
-        const p = document.createElement("p")
+        const title = document.createElement("p")
+        const description = document.createElement("p")
 
+        div.className = "tooltip"
         editButton.className = "edit"
         deleteButton.className = "delete"
         doneButton.className = task.completed ? "undoneButton" : "doneButton"
-        p.className = task.completed ? "undone" : "done"
+        title.className = task.completed ? "undone" : "done"
+        description.className = "tooltipText"
 
         editButton.textContent = "Edit"
         deleteButton.textContent = "Delete"
         doneButton.textContent = task.completed ? "Undone" : "Done"
-        p.textContent = task.title
+        title.textContent = task.title
+        description.textContent = task.description
 
         li.id = task.id
 
-        li.append(p, editButton, deleteButton, doneButton)
+        div.append(title, description)
+        li.append(div, editButton, deleteButton, doneButton)
         list.append(li)
     })
 
