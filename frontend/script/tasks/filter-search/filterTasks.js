41e2a4b3cb60ed1578401b54b6454dba4d07bde2
Kaiky Rodrigues <kaikyro11@gmail.com>
2026-01-21 13:33:47 -0300

    chore(deploy): configure production setup

--- frontend/script/tasks/filter-search/filterTasks.js
+++ frontend/script/tasks/filter-search/filterTasks.js
@@ -2,7 +2,6 @@ import { tasks } from "../counter/state.js"
 import { renderTasks } from "../render/tasks/render.js"
 
 const select = document.querySelector("#select")
-const message = document.querySelector("#message")
 
 export const filterTasks = () => {
     select.addEventListener("change", async () => {
